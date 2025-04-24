abf521445e6a2b284d2b1b2ee153a13ed861069f
masashi.otsubo <designpiece.com@gmail.com>
2025-04-24 15:20:29 +0900

    Simplify postcss.config.js by removing redundant configuration

--- playground/postcss/postcss.config.js
+++ playground/postcss/postcss.config.js
@@ -5,13 +5,7 @@
 module.exports = {
   plugins: [
     // @smsshcss/postcssプラグインを使用
-    require('@smsshcss/postcss')({
-      // 設定ファイルのパスを指定（省略可能）
-      // configFile: './smsshcss.config.js'
-      
-      // 以下の設定はsmsshcss.config.jsに移動しました
-      // content, safelist, legacyMode
-    }),
+    require('@smsshcss/postcss')(),
     
     // 他のプラグイン
     require('autoprefixer')
