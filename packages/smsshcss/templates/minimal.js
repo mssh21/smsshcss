0a43602b0d30f74051890b1212c7e3130b68414a
masashi.otsubo <designpiece.com@gmail.com>
2025-04-25 10:17:14 +0900

    commit message

---
+++ packages/smsshcss/templates/minimal.js
@@ -0,0 +1,23 @@
+/**
+ * SmsshCSS 最小構成設定ファイル
+ */
+
+module.exports = {
+  // スキャン対象のファイル
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  // 常に含めるクラス
+  safelist: [],
+  // base.cssを含める
+  includeBaseCSS: true,
+  // テーマ設定
+  theme: {
+    colors: {
+      primary: '#3366FF',
+      textPrimary: '#333333',
+      backgroundBase: '#FFFFFF'
+    }
+  }
+}; 
