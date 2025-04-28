4dec4595bb23fa246288baccb17078eb545773e4
masashi.otsubo <designpiece.com@gmail.com>
2025-04-28 11:19:47 +0900

    test: lint-staged test

--- packages/test/src/index.ts
+++ packages/test/src/index.ts
@@ -1,6 +1,7 @@
-const test = (param1: string, _param2: number) =>{
-    console.log('test')
-    return 'test'
-}
+const test = (_param1: string, _param2: number): string => {
+  const x = 'test';
+  console.log('test');
+  return x;
+};
 
-export default test 
+export default test;
